b9a45719517b6cf1600640e45c4b56b020d1c4cf
tanimSrabon13 <inamul.ke@gmail.com>
2021-12-08 15:36:43 +0600

    first

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ludu_game/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(LuduGame());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
